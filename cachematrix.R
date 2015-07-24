08e688d6b1cfc832c0fb58c8326d180784aba009
dtaylor222 <davidtaylor222@gmail.com>
2015-07-24 23:31:27 +0100

    Finished assignment with comments in place

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignemnt 2 the file contains all that is required to create 
+## a matrix using functions makeCacheMatrix() and cacheSolve() that returns
+## the inverse of the matrix
 
 ## This function creates a special "matrix" object that can cache its inverse.
 ## in reality this retuns a list of the three functions that are the "methods"
-## of the matrix object object, get() , set() and getinverse()
+## of the matrix object object, get() , set(), setinverse() and getinverse()
 makeCacheMatrix <- function(x = matrix()) {
         
         solution <- NULL # 'solution' is the matrix' inverse
@@ -16,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         
         # sets the inverse of the matrix
-        setinverse <- function(solve) solution <<- solve
+        setinverse <- function(solve) solution <<- solve # still cant quite believe R !
         
         # simlpy returns the matrix' inverse
         getinverse <- function() solution
@@ -31,13 +32,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        result <- x$getinverse()
-        if(!is.null(result)) {
-                message("getting cached data")
+        result <- x$getinverse() # whats currnetly cached ?
+        if(!is.null(result)) {   # something was cached so ...
+                message("getting cached data") # lets us know that this is the cache
                 return(result)
         }
-        data <- x$get()
-        solution <- solve(data, ...)
-        x$setinverse(solution)
+        # nothing is in the cache for us to end here so well ahve to solve and store it
+        data <- x$get() # the data matrix is the actual matix in the object
+        solution <- solve(data, ...)  # solve funtion returns the inverse of data
+        x$setinverse(solution) # This is what actually stores the cache
         solution
 }
